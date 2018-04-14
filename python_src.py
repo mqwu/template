b33df60bc2030245a2ce82c908f09b903e90144d
mqwu <send2mqwu@gmail.com>
2018-04-14 15:55:47 -0500

    Add files via upload

---
+++ python_src.py
@@ -0,0 +1,20 @@
+Structuring Your Project
+
+http://docs.python-guide.org/en/latest/writing/structure/
+
+sparse matrix
+http://code.activestate.com/recipes/52275-sparse-a-dictionary-based-sparse-matrix-class/
+
+structure python code
+http://intermediate-and-advanced-software-carpentry.readthedocs.io/en/latest/structuring-python.html
+
+python project the right way
+https://coderwall.com/p/lt2kew/python-creating-your-project-structure
+https://jeffknupp.com/blog/2013/08/16/open-sourcing-a-python-project-the-right-way/
+http://fouryears.eu/2014/03/19/structure-of-a-python-project/
+http://www.patricksoftwareblog.com/structure-of-a-python-project/
+https://stackoverflow.com/questions/193161/what-is-the-best-project-structure-for-a-python-application
+
+example to structure a simply python project
+http://computing.io/wp/2017/01/structuring-a-simple-python-project/
+https://github.com/samwisehawkins/project
